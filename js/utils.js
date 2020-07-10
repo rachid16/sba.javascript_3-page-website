9104bdebc8fde18dfd403c5df4367f333369dc8d
razeroual <47251928+razeroual@users.noreply.github.com>
2020-07-10 15:57:08 -0400

    done

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,5 @@
 
+document.forms["feedback"]["full-name"].focus();
 
 function ValidateEmail() 
 {
@@ -12,24 +13,13 @@ function ValidateEmail()
 
     if (isString(fullName)){
         alert("You have entered an invalid Name !");
-    }else
-    
-    {
-        alert("You  Name !");
     }
-
 
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(!email.match(mailformat))
     {
         alert("You have entered an invalid email address!");
-        document.feedback-form.email.focus();
-        return false;
     }
-   
-    
-
-    
 
 }
 

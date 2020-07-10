aa2a9869cb9bd8b794b1b0777bb9ced2189f1eab
razeroual <47251928+razeroual@users.noreply.github.com>
2020-07-10 16:45:12 -0400

    added callback function

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,6 @@
 
 document.forms["feedback"]["full-name"].focus();
+var data = new Array();
 
 function ValidateEmail() 
 {
@@ -8,6 +9,17 @@ function ValidateEmail()
     var model = document.forms["feedback"]["model"].value;
     var msg = document.forms["feedback"]["message"].value;
     var rate = document.forms["feedback"]["service"].value;
+
+     addToArray.name = fullName;
+     addToArray.email = email;
+     addToArray.model = model;
+     addToArray.msg = msg;
+     addToArray.rate = rate;
+
+     data.push(addToArray.toArray);
+     
+
+
 
 
 
@@ -37,4 +49,23 @@ function isString(str){
     }
     return result;
 }
-/**/
+
+// call back function
+
+var arr = [5];
+var addToArray = {
+    name:"",
+    email:"xxx@dddd.com",
+    model:"",
+    msg:"",
+    rate:"",
+    toArray: function(){
+        arr[0] = this.name;
+        arr[1] = this.email;
+        arr[2] = this.model;
+        arr[3] = this.msg;
+        arr[4] = this.rate;
+
+        return arr;
+    }
+}
